ed60f813f15492cc365e86d75deafa50f8bc7296
Maciej Skierkowski <maciej@skierkowski.com>
2014-10-06 14:53:00 -0700

    Updating gems and tests

--- spec/compute_spec.rb
+++ spec/compute_spec.rb
@@ -1,30 +1,23 @@
 require 'spec_helper'
 
 describe 'Compute' do
-  it 'can list stuff' do
+  it 'can list servers' do
 
     username = ENV['RACKSPACE_USERNAME']
     api_key  = ENV['RACKSPACE_API_KEY']
 
     service_instance = service_instance('rackspace_compute')
 
-    received_payload = false
-    service_instance.callback = proc do |action_response|
-      if action_response[:payload]
-        received_payload = true
-      end
-    end
-
-    params={
+    params = {
       'username' => username,
       'api_key' => api_key,
       'region' => 'dfw'
     }
 
-    service_instance.call_action('list',params)
-
-    eventually timeout: 10 do
-      received_payload
+    service_instance.test_action('list',params) do
+      expect_return
+      expect_info message:"Retreiving list of servers"
     end
+
   end
 end
