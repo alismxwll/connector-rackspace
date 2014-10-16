4698b5f3e0c3e67dfd52b91e5d390852de700d5d
Maciej Skierkowski <maciej@skierkowski.com>
2014-10-16 12:04:18 -0700

    DRYing tests

--- spec/compute_flavor_spec.rb
+++ spec/compute_flavor_spec.rb
@@ -3,19 +3,9 @@ require 'spec_helper'
 describe 'Compute' do
   describe 'Flavors' do
     it 'can list servers' do
-
-      username = ENV['RACKSPACE_USERNAME']
-      api_key  = ENV['RACKSPACE_API_KEY']
-
       service_instance = service_instance('rackspace_compute_flavors')
 
-      params = {
-        'username' => username,
-        'api_key' => api_key,
-        'region' => 'dfw'
-      }
-
-      service_instance.test_action('list',params) do
+      service_instance.test_action('list',@params) do
         expect_info message:"Retrieving list of flavors"
         expect_return
       end
--- spec/compute_spec.rb
+++ spec/compute_spec.rb
@@ -2,19 +2,18 @@ require 'spec_helper'
 
 describe 'Compute' do
   it 'can list servers' do
-
-    username = ENV['RACKSPACE_USERNAME']
-    api_key  = ENV['RACKSPACE_API_KEY']
-
     service_instance = service_instance('rackspace_compute')
 
-    params = {
-      'username' => username,
-      'api_key' => api_key,
-      'region' => 'dfw'
-    }
+    service_instance.test_action('list',@params) do
+      expect_info message:"Retrieving list of servers"
+      expect_return
+    end
+  end
 
-    service_instance.test_action('list',params) do
+  it 'can get a server' do
+    service_instance = service_instance('rackspace_compute')
+
+    service_instance.test_action('list',@params) do
       expect_info message:"Retrieving list of servers"
       expect_return
     end
